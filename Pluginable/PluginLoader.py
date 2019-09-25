a1ab2cf35178655483cfc0a622099d8448c815a1
Jakub21 <jakub21@mail.com>
2019-09-25 14:15:46 +0200

    Remake plugin loader
    Plugin loader now uses temporary files instead of globa/local scope manipulation. It works better., is more reliable and made code cleaner. Additionally it resolved issue of merged global scopes of plugins (If a variable was defined in one plugin's Scope.py it could be accessed in another one).

--- Pluginable/PluginLoader.py
+++ Pluginable/PluginLoader.py
@@ -1,12 +1,15 @@
 from os import walk
 from Pluginable.Logger import Logger
 from Pluginable.Namespace import Namespace
+from Pluginable.FileManager import ifnmkdir, rmtree
+
 
 class PluginLoader(Logger):
   def __init__(self, prog):
     super().__init__(('Pluginable', self.__class__.__name__), 'pluginable')
     self.prog = prog
     self.directories = []
+    self.target = '_pluginable'
 
   def addDirectory(self, directory):
     self.directories.append(directory)
@@ -29,78 +32,38 @@ class PluginLoader(Logger):
         raise
 
   def loadPlugin(self, directory, pluginKey):
-    scope = directory.replace('\\','/').split('/')[-2]
-    # Read code files
+    scopeName = directory.replace('\\','/').split('/')[-2]
+
     baseDir = f'{directory}{pluginKey}/'
-    scopeFile = open(f'{baseDir}Scope.py').read()
-    configFile = open(f'{baseDir}Config.py').read()
-    pluginFile = open(f'{baseDir}{pluginKey}.py').read()
-    helperFiles = [open(f'{baseDir}{location}').read()
-      for location in next(walk(f'{baseDir}'))[2] if \
-        location[:-3] not in ['Scope', 'Config', pluginKey] and
-        location[0] != location[0].lower() and location.endswith('.py')]
-    taskFiles = [open(f'{baseDir}{location}').read()
-      for location in next(walk(f'{baseDir}'))[2]
-      if location[1] != location[1].lower() and location.endswith('.py')
+    scopeFile = f'{baseDir}Scope.py'
+    configFile = f'{baseDir}Config.py'
+    pluginFile = f'{baseDir}{pluginKey}.py'
+    helperFiles = [f'{baseDir}{location}' for location in next(walk(baseDir))[2]
+        if location[:-3] not in ['Scope', 'Config', pluginKey]
+        and location[0] != location[0].lower()
+        and location.endswith('.py')]
+    taskFiles = [f'{baseDir}{location}' for location in next(walk(baseDir))[2]
+      if location[1] != location[1].lower()
+      and location.endswith('.py')
       and location[0] == '_']
 
-    # Preparations
-    preScopeLocals = list(set(locals().keys()))
-    try: exec(scopeFile)
-    except SyntaxError:
-      self.logError(f'Syntax Error in {scope}::{pluginKey} (Scope)')
-      raise
-    for file in helperFiles:
-      try: exec(file)
-      except SyntaxError:
-        self.logError(f'Syntax Error in {scope}::{pluginKey} (Helper)')
-        raise
-    globals().update({k:v for k, v in locals().items() if k not in preScopeLocals})
+    target = f'./{self.target}/{pluginKey}.py'
+    ifnmkdir(self.target)
+    open(target, 'w+').close()
+    f = open(target, 'a', newline='\n')
+    for file in [scopeFile, configFile] + helperFiles + taskFiles + [pluginFile]:
+      f.write(open(file).read())
+    f.close()
 
-    preExecLocals = list(set(locals().keys()))
+    exec(f'from {self.target}.{pluginKey} import *')
+    rmtree(self.target)
 
-    # Execute code
-    try: exec(configFile)
-    except SyntaxError:
-      self.logError(f'Syntax Error in {scope}::{pluginKey} (Config)')
-      raise
-    try: exec(pluginFile)
-    except SyntaxError:
-      self.logError(f'Syntax Error in {scope}::{pluginKey} (Plugin Core)')
-      raise
-    for file in taskFiles:
-      try: exec(file)
-      except SyntaxError:
-        self.logError(f'Syntax Error in {scope}::{pluginKey} (Task)')
-        raise
-    try: del file
-    except UnboundLocalError: pass
-
-    # Wrap a plugin
-    elements = [k for k in locals().keys() if k not in ['preExecLocals']+preExecLocals]
-    PluginCls = eval(pluginKey)
-    config = eval('Config')
-    helpers = [eval(elm) for elm in elements if elm[0] != elm[0].lower() and \
-      elm not in [pluginKey, 'Config'] + preExecLocals]
-    tasks = []
-    for elm in elements:
-      if elm.startswith('_') and elm[1] != elm[1].lower():
-        tasks += [eval(elm)]
-    PluginCls.scope = scope
-    PluginCls.key = PluginCls.__name__
-    plugin = PluginCls(self.prog)
-    plugin.cnf = config
-    for Helper in helpers:
-      name = Helper.__name__
-      name = name[0].lower() + name[1:]
-      plugin.__dict__[name] = Helper()
-    for task in tasks:
-      key = task.__name__[1:]
-      key = key[0] + key[1:]
-      task.key = key
-      task.plugin = plugin
-      plugin.tasks[key] = task
-    return plugin
+    PluginClass = eval(pluginKey)
+    PluginClass.scope = scopeName
+    PluginClass.key = pluginKey
+    PluginClass.cnf = eval('Config')
+    PluginClass
+    return PluginClass(self.prog)
 
   @staticmethod
   def orderByDependencies(plugins):
