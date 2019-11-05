a01177e2f6cb6297f5eedce276f168af992947bd
Jakub21 <jakub21@mail.com>
2019-11-05 10:18:58 +0100

    Prototype Update

--- Pluginable/__init__.py
+++ Pluginable/__init__.py
@@ -3,3 +3,4 @@ from Pluginable.Namespace import Namespace
 from Pluginable.Plugin import Plugin
 from Pluginable.Task import Task
 from Pluginable.Event import Event
+from Pluginable.Command import Command
