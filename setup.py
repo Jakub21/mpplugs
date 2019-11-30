cf814b135a87714099151e8c4041ab506095bf5d
Jakub21 <jakub21@mail.com>
2019-11-30 16:30:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'Pluginable',
+  description = 'Universal setup+loop framework with multiprocessing support',
+  version = '0.3.0',
+  license = 'MIT',
+  author = 'Jakub21',
+  url = 'https://github.com/Jakub21/Pluginable',
+  packages = find_packages(),
+)
